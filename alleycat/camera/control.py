b7ae9888b575d384a4a5ab188fcd0c112bb042a6
Xavier Cho <mysticfallband@gmail.com>
2021-03-01 19:56:05 +0900

    Slightly cleaner implementation of ThirdPersonCamera

--- alleycat/camera/control.py
+++ alleycat/camera/control.py
@@ -1,40 +1,17 @@
-import math
+from abc import ABC
 from math import radians
 
-from alleycat.reactive import RP, RV, ReactiveObject, functions as rv
-from bge.types import KX_GameObject, KX_PythonComponent
-from dependency_injector.wiring import Provide, inject
-from mathutils import Euler, Matrix, Vector
-from rx import operators as ops
+from bge.types import KX_Camera
+from bpy.types import Camera
+from mathutils import Matrix
 
-from alleycat.game import GameContext
-from alleycat.input import InputMap
-from alleycat.log import LoggingSupport
+from alleycat.common import IDComponent
 
 
-class CameraControl(LoggingSupport, ReactiveObject, KX_PythonComponent):
-    pitch: RP[float] = rv.from_value(0.0).map(lambda _, v: min(max(v, -math.pi), math.pi))
-
-    yaw: RP[float] = rv.from_value(0.0).map(lambda _, v: min(max(v, -math.pi), math.pi))
-
-    rotation: RV[Euler] = rv.combine_latest(pitch, yaw)(ops.map(lambda v: Euler((v[0], 0, -v[1]), "XYZ")))
-
+class CameraControl(IDComponent[KX_Camera, Camera], ABC):
     # noinspection PyUnresolvedReferences
-    base_rotation: Matrix = Matrix.Rotation(radians(180), 3, "Z") @ Matrix.Rotation(radians(90), 3, "X")
+    _base_rotation: Matrix = Matrix.Rotation(radians(180), 3, "Z") @ Matrix.Rotation(radians(90), 3, "X")
 
-    # noinspection PyUnusedLocal
-    def __init__(self, obj: KX_GameObject):
-        super().__init__()
-
-    @inject
-    def start(
-            self,
-            args: dict,
-            input_map: InputMap = Provide[GameContext.input.mappings]) -> None:
-        def rotate(value: Vector):
-            self.pitch += value.y
-            self.yaw += value.x
-
-        rotate_input = input_map["view"]["rotate"]
-
-        rv.observe(rotate_input.value).subscribe(rotate, on_error=self.error_handler)
+    @property
+    def base_rotation(self) -> Matrix:
+        return self._base_rotation
--- alleycat/camera/thirdperson.py
+++ alleycat/camera/thirdperson.py
@@ -1,57 +1,79 @@
 from collections import OrderedDict
+from itertools import chain
+from typing import Final
 
-import bge
-from alleycat.reactive import functions as rv
-from bge.types import KX_GameObject, KX_Scene
+from alleycat.reactive import RP, functions as rv
+from bge.types import KX_GameObject
 from bpy.types import Object
 from dependency_injector.wiring import Provide, inject
 from mathutils import Vector
 from mathutils.geometry import distance_point_to_plane
+from returns.curry import curry, partial
+from rx import Observable
 
 from alleycat.camera import CameraControl
+from alleycat.common import ArgumentReader
+from alleycat.control import TurretControl
 from alleycat.game import GameContext
 from alleycat.input import InputMap
 
 
-class ThirdPersonCamera(CameraControl):
-    args = OrderedDict((
-        ("Pivot", Object),
-        ("Viewpoint", Object),
-    ))
+class ThirdPersonCamera(TurretControl, CameraControl):
+    class ArgKeys(TurretControl.ArgKeys):
+        ZOOM_INPUT: Final = "Zoom Input"
+        ZOOM_SENSITIVITY: Final = "Zoom Sensitivity"
+        PIVOT: Final = "Pivot"
+        VIEWPOINT: Final = "Viewpoint"
 
-    def __init__(self, obj: KX_GameObject):
-        super().__init__(obj)
+    args = OrderedDict(chain(TurretControl.args.items(), (
+        (ArgKeys.ZOOM_INPUT, "view/zoom"),
+        (ArgKeys.ZOOM_SENSITIVITY, 1.0),
+        (ArgKeys.PIVOT, Object),
+        (ArgKeys.VIEWPOINT, Object),
+    )))
+
+    distance: RP[float] = rv.from_value(1.0).map(lambda _, v: max(v, 0.0))
 
     @inject
     def start(self, args: dict, input_map: InputMap = Provide[GameContext.input.mappings]) -> None:
         super().start(args)
 
-        scene: KX_Scene = bge.logic.getCurrentScene()
+        props = ArgumentReader(args)
 
-        self.pivot: KX_GameObject = scene.getGameObjectFromObject(args["Pivot"])
-        self.viewpoint: KX_GameObject = scene.getGameObjectFromObject(args["Viewpoint"])
+        # noinspection PyTypeChecker,PyShadowingBuiltins
+        input = props.require(self.ArgKeys.ZOOM_INPUT, str) \
+            .map(lambda s: s.split("/")) \
+            .bind(input_map.observe)
 
-        self.logger.info("Input map: %s", input_map)
-        self.logger.info("Pivot object: %s", self.pivot)
-
-        self.distance = 1.0
+        pivot = props.require(self.ArgKeys.PIVOT, Object).map(self.as_game_object)
+        sensitivity = props.read(self.ArgKeys.ZOOM_SENSITIVITY, float).value_or(1.0)
+        viewpoint = props.read(self.ArgKeys.VIEWPOINT, Object).map(self.as_game_object)
 
         def zoom(value: float):
-            self.distance = min(max(self.distance - value * 0.07, 0), 10)
+            self.distance = max(self.distance - value * 0.1 * sensitivity, 0)
 
-        zoom_input = input_map["view"]["zoom"]
+        def init(i: Observable, p: KX_GameObject):
+            self.callbacks.append(partial(self.process, p, viewpoint.value_or(p)))
 
-        rv.observe(zoom_input.value).subscribe(zoom, on_error=self.error_handler)
+            i.subscribe(zoom, on_error=self.error_handler)
 
-    def update(self) -> None:
-        up_axis = self.pivot.worldOrientation @ Vector((0, 0, 1))
+        init = input.bind(lambda i: pivot.map(lambda p: init(i, p)))
 
-        height = distance_point_to_plane(self.viewpoint.worldPosition, self.pivot.worldPosition, up_axis)
+        init.alt(self.logger.warning)
+
+    @curry
+    def process(self, pivot: KX_GameObject, viewpoint: KX_GameObject) -> None:
+        assert pivot
+        assert viewpoint
+
+        up_axis = pivot.worldOrientation @ Vector((0, 0, 1))
+
+        height = distance_point_to_plane(viewpoint.worldPosition, pivot.worldPosition, up_axis)
 
         mat = self.rotation.to_matrix()
 
-        orientation = self.pivot.worldOrientation @ mat @ self.base_rotation
-        offset = self.pivot.worldOrientation @ mat @ Vector((0, -1, 0)) * self.distance
+        orientation = pivot.worldOrientation @ mat @ self.base_rotation
+        offset = pivot.worldOrientation @ mat @ Vector((0, -1, 0)) * self.distance
 
         self.object.worldOrientation = orientation
-        self.object.worldPosition = self.pivot.worldPosition - offset + up_axis * height * 0.8
+        self.object.worldPosition = pivot.worldPosition - offset + up_axis * height * 0.8
