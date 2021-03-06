e904c6e02a1acd56bdd2d50634b2d055dfe93289
Xavier Cho <mysticfallband@gmail.com>
2021-03-06 11:10:28 +0900

    Add normalize_angle function

--- alleycat/math/__init__.py
+++ alleycat/math/__init__.py
@@ -1,4 +1,15 @@
+import math
+
 from validator_collection import validators
+
+
+def normalize_angle(angle: float) -> float:
+    angle = validators.float(angle) % (math.pi * 2)
+
+    if angle > math.pi:
+        angle -= math.pi * 2
+
+    return angle
 
 
 def normalize_euler(angle: float) -> float:
--- tests/math/test_module.py
+++ tests/math/test_module.py
@@ -1,9 +1,18 @@
-from pytest import mark
+from math import radians
 
-from alleycat.math import normalize_euler
+from pytest import approx, mark
+
+from alleycat.math import normalize_angle, normalize_euler
+
+fixture = [
+    (0, 0), (90, 90), (180, 180), (270, -90), (-0, 0), (-90, -90), (-180, 180), (-270, 90), (450, 90), (-450, -90)]
 
 
-@mark.parametrize("angle, expected", [
-    (0, 0), (90, 90), (180, 180), (270, -90), (-0, 0), (-90, -90), (-180, 180), (-270, 90), (450, 90), (-450, -90)])
+@mark.parametrize("angle, expected", fixture)
+def test_normalize_angle(angle, expected):
+    assert normalize_angle(radians(angle)) == approx(radians(expected))
+
+
+@mark.parametrize("angle, expected", fixture)
 def test_normalize_euler(angle, expected):
     assert normalize_euler(angle) == expected
