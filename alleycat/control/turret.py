23d7613fb5cd04483a7da73eafeeeb0c9eb74782
Xavier Cho <mysticfallband@gmail.com>
2021-03-06 18:33:37 +0900

    Allow turret-like control to be toggled on and off

--- alleycat/control/turret.py
+++ alleycat/control/turret.py
@@ -1,6 +1,7 @@
 import math
 from abc import ABC
 from collections import OrderedDict
+from itertools import chain
 from typing import Final
 
 from alleycat.reactive import RP, RV, functions as rv
@@ -8,21 +9,21 @@ from dependency_injector.wiring import Provide, inject
 from mathutils import Euler, Vector
 from rx import operators as ops
 
-from alleycat.common import ArgumentReader, BaseComponent
+from alleycat.common import ActivatableComponent, ArgumentReader
 from alleycat.game import GameContext
 from alleycat.input import InputMap
 from alleycat.math import clamp, normalize_angle
 
 
-class TurretControl(BaseComponent, ABC):
-    class ArgKeys:
+class TurretControl(ActivatableComponent, ABC):
+    class ArgKeys(ActivatableComponent.ArgKeys):
         ROTATION_INPUT: Final = "Rotation Input"
         ROTATION_SENSITIVITY: Final = "Rotation Sensitivity"
 
-    args = OrderedDict((
+    args = OrderedDict(chain(ActivatableComponent.args.items(), (
         (ArgKeys.ROTATION_INPUT, "view/rotate"),
         (ArgKeys.ROTATION_SENSITIVITY, 1.0)
-    ))
+    )))
 
     pitch: RP[float] = rv.from_value(0.0).validate(lambda _, v: clamp(v, -math.pi / 2, math.pi / 2))
 
@@ -36,6 +37,8 @@ class TurretControl(BaseComponent, ABC):
             self,
             args: dict,
             input_map: InputMap = Provide[GameContext.input.mappings]) -> None:
+        super().start(args)
+
         props = ArgumentReader(args)
 
         sensitivity = props.read(self.ArgKeys.ROTATION_SENSITIVITY, float).value_or(1.0)
@@ -48,5 +51,5 @@ class TurretControl(BaseComponent, ABC):
         props.require(self.ArgKeys.ROTATION_INPUT, str) \
             .map(lambda s: s.split("/")) \
             .bind(input_map.observe) \
-            .map(lambda o: o.subscribe(rotate, on_error=self.error_handler)) \
+            .map(lambda o: o.pipe(ops.filter(lambda _: self.active)).subscribe(rotate, on_error=self.error_handler)) \
             .alt(self.logger.warning)
