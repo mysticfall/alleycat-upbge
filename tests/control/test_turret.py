c568c8180aeb3939dbb4689c0cceeaec9e4ea69c
Xavier Cho <mysticfallband@gmail.com>
2021-04-03 17:38:12 +0900

    Fix test failure

--- tests/control/test_turret.py
+++ tests/control/test_turret.py
@@ -1,11 +1,12 @@
 from unittest.mock import patch
 
 from alleycat.reactive import RP, RV, functions as rv
+from bge.types import KX_GameObject
 from pytest import approx
 from pytest_mock import MockerFixture
 
-from alleycat.common import Lookup
-from alleycat.input import InputBinding, InputMap
+from alleycat.common import ActivatableComponent
+from alleycat.input import InputBinding
 from tests import MockEuler, MockVector
 
 
@@ -21,28 +22,22 @@ class TestBinding(InputBinding[MockVector]):
 @patch("mathutils.Vector", MockVector)
 @patch("mathutils.Euler", MockEuler)
 def test_start(mocker: MockerFixture):
-    obj = mocker.patch("bge.types.KX_GameObject")
-
     binding = TestBinding("rotate")
-    input_map = InputMap({"view": Lookup({"rotate": binding})})
 
     from alleycat.control import TurretControl
 
-    class TestControl(TurretControl):
-        pass
+    class TestControl(TurretControl[KX_GameObject]):
+        def __init__(self, obj: KX_GameObject):
+            obj.name = "test"
+
+            super().__init__(obj)
 
     args = dict([
-        (TurretControl.ArgKeys.ROTATION_INPUT, "view/rotate"),
-        (TurretControl.ArgKeys.ROTATION_SENSITIVITY, 2.0)
+        (ActivatableComponent.ArgKeys.ACTIVE, True),
     ])
 
-    control = TestControl(obj)
-    control.start(args, input_map)
+    control = TestControl(mocker.patch("bge.types.KX_GameObject"))
+    control.start(args)
 
     assert control.pitch == approx(0.0)
     assert control.yaw == approx(0.0)
-
-    binding.current = MockVector((0.3, 0.2))
-
-    assert control.pitch == approx(0.4)
-    assert control.yaw == approx(0.6)
